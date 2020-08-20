e4839505a6f5a57cd81a8c84c40185a9e066f5eb
mholyaee <mh.olyaee@gmail.com>
2020-08-20 11:10:35 +0430

    Add files via upload

--- HGHapCut_mec_RAX.m
+++ HGHapCut_mec_RAX.m
@@ -1,4 +1,4 @@
-function [H_rec]=HGHapCut_mec_RAX(varargin)
+function [H_assem2]=HGHapCut_mec_RAX(varargin)
 clc
 if nargin==0
     Mpath=uigetdir('D:\Bioinformatics\HapData\','Select Dataset');
@@ -33,8 +33,10 @@ for d=1:Num_paras
         fprintf('Succeed to find %s \\*.m.4.err!\n',D1(d).name);
         Num_exps=length(D);
         sss=D1(d).name;
+        Num_exps=10;
     end
     for i=1:Num_exps  %for each instance
+        tStart=tic;
         if nargin==0
         [M0,m0,n0,H_real]=inputdata(Mpath,D1(d).name,D(i).name,i);
         else
@@ -47,7 +49,7 @@ for d=1:Num_paras
         BestHap=H_rec;
         K=5;  %  parameter k in the SkNN, It can be changed.
         C=2;   %  parameter cs the support count threshold, one can change it.
-        t=100;
+        t=50;
         j=1;
         thr=0;
         Nparts=2;% Number of partitions
@@ -61,7 +63,7 @@ for d=1:Num_paras
         pre=0;
         MinMEC=100000;
         BestCost=100000;
-        while (j<=t && thr<=50 && MinMEC>0)
+        while (j<=t && thr<=10 && MinMEC>0)
             clc;
             fprintf('Sample:%d\t iter:%d\t UBfactor:%d\t thr:%d\n',i,j,UBfactor,thr);
             RNNname1=MakeKNNcpp_RA(M,m,n,K,i,H_rec,sss,ReadAssign);% for haplotype phasing
@@ -93,18 +95,16 @@ for d=1:Num_paras
             [newHap2,Confidence]=MakeHapReadAssignGeraci(M,ReadAssign);
             [t3]=MEC(newHap2,M);
             j=j+1;
-            if t3<t1
+            if t3<t2
                 H_rec=newHap2;
-                thr=0;
                 MinMEC=t3;
                 if MinMEC<BestCost
                     BestCost=MinMEC;
                     BestHap=newHap2;
+                    thr=0;
+                else
+                    thr=thr+1;
                 end
-            elseif t3==pre
-                thr=thr+1;
-            else
-                thr=0;
             end
             if MinMEC>=t2
                 if UBfactor<=50
@@ -113,11 +113,10 @@ for d=1:Num_paras
                 
             end
         end
+        tElapsed(i)=toc(tStart);
         H_assem1=CompleteHap(H_rec(1,:),H_assem);
         H_assem4=H_assem2to4(H_assem1,Most);
         rrs(1,i)=RR4(H_real,H_assem4);
-        fprintf('The instance %d is finished!....RR=%f\n',i,rrs(1,i));
-        
         H_assem2=CompleteHap(BestHap(1,:),H_assem);
         H_assem4=H_assem2to4(H_assem2,Most);
         rrs(2,i)=RR4(H_real,H_assem4);
@@ -130,14 +129,15 @@ for d=1:Num_paras
     end %end for each instance
     %save path
     if nargin==0
-        path=[pwd,'\results\'];        
+        path=[pwd,'\newresults\'];        
         fid=fopen([path,D1(d).name,'_RR.txt'],'w+');
         for j=1:Num_exps
-            fprintf(fid,'%d\t%1.4f\t%1.4f\n',j,rrs(1,j),rrs(2,j));
+            fprintf(fid,'%d\t%1.4f\t%1.4f\t%1.4f\n',j,rrs(1,j),rrs(2,j),tElapsed(j));
         end
         average_rr=mean(rrs(1,:));
         average_rr2=mean(rrs(2,:));
-        fprintf(fid,'mean1=%1.4f\t mean1=%1.4f',average_rr,average_rr2);
+        average_t=mean(tElapsed);
+        fprintf(fid,'mean1=%1.4f\t mean1=%1.4f Time=%1.4f',average_rr,average_rr2,average_t);
         fclose(fid);
     end
 end %end for each parameter set
